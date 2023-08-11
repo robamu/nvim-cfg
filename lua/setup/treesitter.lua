2d2491bfba43c4e9708fdeb072bb42627bc13c77
Robin Mueller <robin.mueller.m@gmail.com>
2023-08-11 12:03:09 +0200

    what the hell is <M-Space>?

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -17,7 +17,7 @@ require('nvim-treesitter.configs').setup {
       init_selection = '<c-space>',
       node_incremental = '<c-space>',
       scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
+      node_decremental = '<c-r>',
     },
   },
   textobjects = {
