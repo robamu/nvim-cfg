874396745b317be39c91d4dca8db362bb8bff8e4
Robin Mueller <robin.mueller.m@gmail.com>
2023-09-03 18:45:28 +0200

    almost working

--- lua/setup/rust-tools.lua
+++ lua/setup/rust-tools.lua
@@ -4,9 +4,22 @@ local rt = require("rust-tools")
 local mason_registry = require("mason-registry")
 
 local codelldb = mason_registry.get_package("codelldb")
-local extension_path = codelldb:get_install_path() .. "/extension"
+-- Copied from https://github.com/simrat39/rust-tools.nvim/wiki/Debugging
+-- Adapted for mason
+local extension_path = codelldb:get_install_path() .. "/extension/"
 local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+-- This is the shared object for Ubuntu.. might be different on different OSes
+local liblldb_path = extension_path .. "lldb/lib/liblldb"
+local this_os = vim.loop.os_uname().sysname;
+
+-- The path in windows is different
+if this_os:find "Windows" then
+  codelldb_path = extension_path .. "adapter\\codelldb.exe"
+  liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+else
+  -- The liblldb extension is .so for linux and .dylib for macOS
+  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+end
 
 -- Configure LSP through rust-tools.nvim plugin.
 -- rust-tools will configure and enable certain LSP features for us.
@@ -39,6 +52,7 @@ rt.setup({
 
   dap = {
     adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+
   }
 })
 
