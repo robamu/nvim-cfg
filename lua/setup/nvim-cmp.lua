67b1dbc07b9ac022672101c90018bfe295141ef8
Robin Mueller <robin.mueller.m@gmail.com>
2023-09-03 21:19:15 +0200

    add back confirmation

--- lua/setup/nvim-cmp.lua
+++ lua/setup/nvim-cmp.lua
@@ -20,7 +20,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm({
-      -- behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
     ['<Tab>'] = cmp.mapping(function(fallback)
