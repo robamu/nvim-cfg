ea7bb2caea85a74c372225e42112c34c15a8a26c
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2023-09-04 17:57:01 +0200

    that somehow breaks python?

--- lua/setup/pyright-lsp.lua
+++ lua/setup/pyright-lsp.lua
@@ -1,6 +1,11 @@
 local generic_cfg = require("setup/generic-lspcfg")
 
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 require("lspconfig").pyright.setup {
+  capabilities = capabilities,
   on_attach = function(_, bufnr)
     generic_cfg.on_attach(_, bufnr)
   end
