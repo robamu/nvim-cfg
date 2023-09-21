70f6ad96d08a7a9fe0f1accb9e498f725be724b9
Robin Mueller <robin.mueller.m@gmail.com>
2023-09-21 17:37:28 +0200

    better toggleterm settings

--- lua/setup/toggleterm.lua
+++ lua/setup/toggleterm.lua
@@ -1,1 +1,5 @@
-require("toggleterm").setup {}
+local tt = require("toggleterm")
+tt.setup {}
+
+vim.keymap.set('n', '<leader>tt', tt.toggle_command, { desc = "[T]oggle [T]erminal"})
+vim.keymap.set('t', '<leader><esc>', [[<C-\><C-n>]], { desc = "Exit terminal"})
