56ecc7686613fa72852d81a360e960d57b844b0e
Robin Mueller <robin.mueller.m@gmail.com>
2023-10-09 10:45:53 +0200

    remove custom fmt command for python

--- lua/setup/python-lsps.lua
+++ lua/setup/python-lsps.lua
@@ -15,8 +15,5 @@ require("lspconfig").ruff_lsp.setup {
   on_attach = function(client, bufnr)
     generic_cfg.on_attach(client, bufnr)
     client.server_capabilities.hoverProvider = false
-    vim.keymap.set('n', "<Leader>f", "<cmd>call Black()<CR>",{
-      buffer = bufnr, desc = "[F]ormat using Black"
-    })
   end
 }
