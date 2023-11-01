d4acc8f78d010bfcfe516a24929318a0ee455e81
Robin Mueller <robin.mueller.m@gmail.com>
2023-11-01 17:40:36 +0100

    lets try pylsp

--- lua/setup/python-lsps.lua
+++ lua/setup/python-lsps.lua
@@ -4,11 +4,26 @@ local generic_cfg = require("setup/generic-lspcfg")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-require("lspconfig").pyright.setup({
+-- require("lspconfig").pyright.setup({
+--   capabilities = capabilities,
+--   on_attach = function(_, bufnr)
+--     generic_cfg.on_attach(_, bufnr)
+--   end,
+-- })
+
+require("lspconfig").pylsp.setup({
   capabilities = capabilities,
-  on_attach = function(_, bufnr)
-    generic_cfg.on_attach(_, bufnr)
+  on_attach = function(client, bufnr)
+    generic_cfg.on_attach(client, bufnr)
   end,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = false },
+        flake8 = { enabled = false },
+      },
+    },
+  },
 })
 
 require("lspconfig").ruff_lsp.setup({
