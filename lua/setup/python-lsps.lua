2c2dd4ff633ef2612b62d02c09ddc1316aed4d6c
Robin Mueller <robin.mueller.m@gmail.com>
2023-11-10 12:54:20 +0100

    fmt

--- lua/setup/python-lsps.lua
+++ lua/setup/python-lsps.lua
@@ -6,9 +6,9 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 require("lspconfig").pyright.setup({
   capabilities = capabilities,
-   on_attach = function(_, bufnr)
-     generic_cfg.on_attach(_, bufnr)
-   end,
+  on_attach = function(_, bufnr)
+    generic_cfg.on_attach(_, bufnr)
+  end,
 })
 
 --require("lspconfig").pylsp.setup({
