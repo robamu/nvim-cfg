15d1c687bbd02343346c778b2a77a1b99cb8edda
Robin Mueller <robin.mueller.m@gmail.com>
2023-11-10 12:53:06 +0100

    go back to pyright..

--- lua/setup/python-lsps.lua
+++ lua/setup/python-lsps.lua
@@ -4,28 +4,28 @@ local generic_cfg = require("setup/generic-lspcfg")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
--- require("lspconfig").pyright.setup({
---   capabilities = capabilities,
---   on_attach = function(_, bufnr)
---     generic_cfg.on_attach(_, bufnr)
---   end,
--- })
-
-require("lspconfig").pylsp.setup({
+require("lspconfig").pyright.setup({
   capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    generic_cfg.on_attach(client, bufnr)
-  end,
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = { enabled = false },
-        flake8 = { enabled = false },
-      },
-      rope_autoimport = { enabled = true },
-    },
-  },
+   on_attach = function(_, bufnr)
+     generic_cfg.on_attach(_, bufnr)
+   end,
 })
+
+--require("lspconfig").pylsp.setup({
+--  capabilities = capabilities,
+--  on_attach = function(client, bufnr)
+--    generic_cfg.on_attach(client, bufnr)
+--  end,
+--  settings = {
+--    pylsp = {
+--      plugins = {
+--        pycodestyle = { enabled = false },
+--        flake8 = { enabled = false },
+--      },
+--      rope_autoimport = { enabled = true },
+--    },
+--  },
+--})
 
 require("lspconfig").ruff_lsp.setup({
   on_attach = function(client, bufnr)
