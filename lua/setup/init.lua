10107b30d8f7fbf878fb0419dc7362eb122cecb2
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2023-11-30 18:30:31 +0100

    something broke here..

--- lua/setup/init.lua
+++ lua/setup/init.lua
@@ -1,16 +1,17 @@
 require("setup/theme")
--- neodev must come before the LSP configuration
---
--- The generic mason LSP config must come before all other individual LSP configs.
+
+-- neodev must come before the LSP configuration.
+require("setup/neodev")
 require("setup/lsps")
 
+require("setup/dap-config")
 require("setup/python-dap")
+
 require("setup/venv-select")
 require("setup/autopairs")
 require("setup/nvim-cmp")
 require("setup/telescope")
 require("setup/treesitter")
-require("setup/dap-config")
 require("setup/web-devicons")
 require("setup/lualine")
 require("setup/toggleterm")
--- lua/setup/lsps/init.lua
+++ lua/setup/lsps/init.lua
@@ -1,5 +1,4 @@
 require("setup/lsps/mason-lsp")
 require("setup/lsps/webdev-lsps")
 require("setup/lsps/python-lsps")
-require("setup/lsps/neodev")
 require("setup/lsps/rust-tools")
