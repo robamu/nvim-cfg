a47bb6477c7cfd0591186d0baa3b915028580e92
Robin Mueller <robin.mueller.m@gmail.com>
2023-12-04 12:13:12 +0100

    this is kind of confusing

--- lua/setup/nvim-cmp.lua
+++ lua/setup/nvim-cmp.lua
@@ -27,9 +27,12 @@ cmp.setup({
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<C-y>"] = cmp.mapping.confirm({
       select = true,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
