4c6ef19fa8681445bb912934d7fb874143605755
Robin Mueller <robin.mueller.m@gmail.com>
2024-04-20 10:33:09 +0200

    hopefully this works

--- lua/setup/nvim-cmp.lua
+++ lua/setup/nvim-cmp.lua
@@ -29,6 +29,7 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     }),
     ["<C-x>"] = cmp.mapping.confirm({
