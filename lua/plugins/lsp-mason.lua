0185a982bb959f4d3dbac2992a090186b9379b73
Robin Mueller <robin.mueller.m@gmail.com>
2024-09-10 11:05:43 +0200

    update for python

--- lua/plugins/lsp-mason.lua
+++ lua/plugins/lsp-mason.lua
@@ -5,6 +5,41 @@ return {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
     },
+    config = function()
+      local generic_cfg = require("plugins/helpers/generic-lsp")
+
+      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+
+      require("lspconfig").pyright.setup({
+        capabilities = generic_cfg.capabilities,
+        on_attach = function(_, bufnr)
+          generic_cfg.on_attach(_, bufnr)
+        end,
+      })
+
+      --require("lspconfig").pylsp.setup({
+      --  capabilities = capabilities,
+      --  on_attach = function(client, bufnr)
+      --    generic_cfg.on_attach(client, bufnr)
+      --  end,
+      --  settings = {
+      --    pylsp = {
+      --      plugins = {
+      --        pycodestyle = { enabled = false },
+      --        rope_autoimport = { enabled = true },
+      --        flake8 = { enabled = false },
+      --      },
+      --    },
+      --  },
+      --})
+
+      require("lspconfig").ruff_lsp.setup({
+        on_attach = function(client, bufnr)
+          generic_cfg.on_attach(client, bufnr)
+          client.server_capabilities.hoverProvider = false
+        end,
+      })
+    end,
   },
   {
     "williamboman/mason.nvim",
