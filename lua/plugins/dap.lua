039e88301af2030a7efa637f560702aba4c573de
Robin Mueller <robin.mueller.m@gmail.com>
2025-05-12 10:17:11 +0200

    cppdbg setup must be updated

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -26,15 +26,15 @@ return {
       -- Use lldb installed by mason. Run MasonInstall codelldb manually.
       -- dap.adapters.codelldb = require("setup/codelldb_cfg").codelldb
 
-      local mason_registry = require("mason-registry")
-      local cpptools = mason_registry.get_package("cpptools")
-      local extensions_path = cpptools:get_install_path() .. "/extension"
-      local cppdbg_cmd = extensions_path .. "/debugAdapters/bin/OpenDebugAD7"
-      dap.adapters.cppdbg = {
-        id = "cppdbg",
-        type = "executable",
-        command = cppdbg_cmd,
-      }
+      -- local mason_registry = require("mason-registry")
+      -- local cpptools = mason_registry.get_package("cpptools")
+      -- local extensions_path = cpptools:get_install_path() .. "/extension"
+      -- local cppdbg_cmd = extensions_path .. "$MASON/share/cpptools/extension/debugAdapters/bin/OpenDebugAD7"
+      -- dap.adapters.cppdbg = {
+        -- id = "cppdbg",
+        -- type = "executable",
+        -- command = cppdbg_cmd,
+      -- }
 
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
