516d9278a15dbf774062c06e8040b9d9d276e46e
Robin Mueller <robin.mueller.m@pm.me>
2025-10-23 11:11:27 +0200

    make C/CPP source header switch work again

--- lua/plugins/lsp-mason.lua
+++ lua/plugins/lsp-mason.lua
@@ -6,23 +6,13 @@ return {
       "mason.nvim",
       { "mason-org/mason-lspconfig.nvim", config = function() end },
     },
-    --opts = {
-    --  setup = {
-    --    -- Prevent configuration here, is done by rustaceanvim.
-    --    rust_analyzer = function()
-    --      return true
-    --    end,
-    --  },
-    --},
     config = function()
       local generic_cfg = require("plugins/helpers/generic-lsp")
-
-      -- lspconfig.pyright.setup({
-      --   capabilities = generic_cfg.capabilities,
-      --   on_attach = function(_, bufnr)
-      --     generic_cfg.on_attach(_, bufnr)
-      --   end,
-      -- })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          generic_cfg.on_attach(ev.data.id, ev.buf)
+        end,
+      })
 
       vim.lsp.config("basedpyright", {
         on_attach = function(_, bufnr)
@@ -39,6 +29,7 @@ return {
         },
       })
       vim.lsp.enable("basedpyright")
+
       vim.lsp.config("ruff", {
         on_attach = function(client, bufnr)
           generic_cfg.on_attach(client, bufnr)
@@ -47,18 +38,25 @@ return {
       })
       vim.lsp.enable("ruff")
 
+      local clangd_config = vim.lsp.config["clangd"].on_attach
       vim.lsp.config("clangd", {
         capabilities = generic_cfg.capabilities,
-        on_attach = function(_, bufnr)
-          generic_cfg.on_attach(_, bufnr)
-          generic_cfg.nmap(
+        on_attach = function(client, bufnr)
+          if (clangd_config ~= nil) then
+            clangd_config(client, bufnr)
+          end
+          vim.keymap.set(
+            "n",
             "<leader><tab>",
-            "<Cmd>ClangdSwitchSourceHeader<CR>",
-            "Switch to source or header file"
+            "<Cmd>LspClangdSwitchSourceHeader<CR>",
+            { desc = "Switch to source or header file" }
           )
         end,
       })
       vim.lsp.enable("clangd")
+
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.enable("lua_ls")
     end,
   },
 
